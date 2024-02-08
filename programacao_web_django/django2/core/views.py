837c8de049e3632da1711158602b426a0461f07c
Luiz Henrique Carneiro Carvalho <luizhenriqueccarvalho@gmail.com>
2024-02-08 15:24:26 -0300

    form

--- programacao_web_django/django2/core/views.py
+++ programacao_web_django/django2/core/views.py
@@ -1,11 +1,39 @@
 from django.shortcuts import render
+from django.contrib import messages
+
+from .forms import ContatoForm
 
 def index(request):
     return render(request, 'index.html')
 
 
 def contato(request):
-    return render(request, 'contato.html')
+    form = ContatoForm(request.POST or None)
+
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+
+            print('Mensagem enviada')
+            print(f'Nome: {nome}')
+            print(f'Email: {email}')
+            print(f'Assunto: {assunto}')
+            print(f'Mensagem: {mensagem}')
+
+            messages.success(request, 'E-mail enviado com sucesso!')
+            form = ContatoForm()
+        else:
+            messages.error(request, 'Erro ao enviar e-mail')
+
+
+    context = {
+        'form' : form
+    }
+
+    return render(request, 'contato.html', context)
 
 
 def produto(request):
