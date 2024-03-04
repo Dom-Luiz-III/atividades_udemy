5cfca04d7e0315e706355b5fb96295b817c94e11
Luiz Henrique Carneiro Carvalho <luizhenriqueccarvalho@gmail.com>
2024-03-04 15:55:30 -0300

    dj_static

--- programacao_web_django/django2/django2/wsgi.py
+++ programacao_web_django/django2/django2/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+# Comando para usar junto com o dj_static
+# from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django2.settings')
+
+# application = Cling(MediaCling(get_wsgi_application()))
 
 application = get_wsgi_application()
