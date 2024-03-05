92763f74720d9d07b858693dc8682f3f44c30f42
Luiz Henrique Carneiro Carvalho <luizhenriqueccarvalho@gmail.com>
2024-03-05 16:45:38 -0300

    Update settings.py

--- programacao_web_django/fusion/fusion/settings.py
+++ programacao_web_django/fusion/fusion/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'fusion',
-        'USER': 'root',
+        'USER': 'postgres',
         'PASSWORD': 'luiz',
         'HOST': 'localhost',
         'PORT': '5432',
